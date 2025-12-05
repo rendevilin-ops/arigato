9a35f7a78a35585dbde3857306aa0b48b89a9227
rendevilin-ops <rendevilin@gmail.com>
2025-12-05 19:52:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showStep(n) {
 const selected = { date: null, service: null, time: null, pax: null };
 
 const apiUrl =
-    "https://script.google.com/macros/s/AKfycbwyZ75zYG6MVcjqh51Vz6bZt7W4LrYYm5SY4WClBM2R03u0Dmmc5RWzC_YP6W2K5FtolA/exec";
+    "https://script.google.com/macros/s/AKfycbzeh8tUBW0xQhBiMHdaNAqTx_8cRQDYYbbJrkoElfH4HDK8iswhpdajEzDLrXjWC_5FFw/exec";
 
 /* ========== Step1 初期化 ========== */
 function setToday() {
@@ -265,6 +265,7 @@ document.getElementById("sendReservation").onclick = async () => {
         showStep(5);
     }
 };
+
 
 
 
